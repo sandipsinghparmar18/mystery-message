9a281f6e542255ffb0515e851cd8f5369f172ad8
Sandip Singh Parmar <sandipsinghparmar18@gmail.com>
2025-09-24 10:21:42 +0530

    modify footed

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -56,7 +56,7 @@ const HomePage = () => {
 
       {/* Footer */}
       <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
-        © 2023 True Feedback. All rights reserved.
+        © 2025 True Feedback Made with ❤️. All rights reserved.
       </footer>
     </>
   );
